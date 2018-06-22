7a5e360e8e038319120cef2ce2bc048978ca8f09
Roberto Barros <roberto.f.barros@gmail.com>
2018-06-22 19:48:33 -0300

    Comments

--- frequencies.rb
+++ frequencies.rb
@@ -1,12 +1,19 @@
 def frequencies(text)
+
+  # split the words
   words = text.split(" ")
+
   result = {}
   words.each do |word|
+    # If word is in the result hash...
     if result.key?(word)
+      # Increment the word count
       result[word] += 1
     else
+      # First time the word is found
       result[word] = 1
     end
   end
-    return result
+
+  return result
 end
--- interface.rb
+++ interface.rb
@@ -1,7 +1,15 @@
 require_relative "./frequencies"
+
+# Read the article file
 neymar = File.read ("neymar.txt")
+
+# 1. Calculate the frequencies
+# 2. Sort by values ascendent
+# 3. Reverse for values descendent
+# 4. Get the top 5 values
 results = frequencies(neymar).sort_by {|_key, value| value}.reverse.first(5)
+
+# Print the top 5 words by frequencies
 results.each do |result|
   puts "A palavra #{result[0]} aparece #{result[1]} vezes."
-
-  end
+end
